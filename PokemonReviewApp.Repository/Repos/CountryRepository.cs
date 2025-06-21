e2b39e94a54e40d307dea79bd3c8b15b1bc899c2
arno huygelier <arno2003.huygelier1@gmail.com>
2025-06-22 00:26:21 +0200

    start with country actions

---
+++ PokemonReviewApp.Repository/Repos/CountryRepository.cs
@@ -0,0 +1,46 @@
+﻿using PokemonReviewApp.Core.Interfaces;
+using PokemonReviewApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonReviewApp.Repository.Repos
+{
+    public class CountryRepository : ICountryRepository
+    {
+        private readonly PokemonDbContext _context;
+
+        public CountryRepository(PokemonDbContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<Country> GetCountries()
+        {
+            return _context.Countries.ToList();
+        }
+
+        public Country? GetCountry(int countryId)
+        {
+            return _context.Countries.Where(c => c.Id == countryId).FirstOrDefault();
+        }
+
+        public Country? GetCountryByOwner(int ownerId)
+        {
+            return _context.Owners.Where(o => o.Id == ownerId).Select(c => c.Country).FirstOrDefault();
+        }
+
+        public ICollection<Owner> GetOwnersFromCountry(int countryId)
+        {
+            return _context.Owners.Where(c => c.Country.Id == countryId).ToList();
+        }
+        
+        public bool CoutryExists(int countryId)
+        {
+            return _context.Countries.Any(c => c.Id == countryId);
+        }
+
+    }
+}
